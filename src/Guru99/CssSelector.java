d6c3c26f5725aae44a3badd1a69395b696dc317c
Prasad <prasad03t@outlook.com>
2025-01-10 23:23:32 +0530

    Second Commit

--- src/Guru99/CssSelector.java
+++ src/Guru99/CssSelector.java
@@ -9,12 +9,14 @@ public class CssSelector {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\prasa\\eclipse\\chromedriver.exe");
+		
 		 WebDriver driver = new ChromeDriver();
 		// driver.get("https://www.facebook.com/");
 		 //System.out.println(driver.findElement(By.cssSelector("input#email")).getTagName());
 		 //System.out.println(driver.findElement(By.cssSelector("input[name=\"email\"]")).getSize());
 		 //System.out.println(driver.findElement(By.cssSelector("input.class[class=\"inputtext _55r1 _6luy _9npi\"]")).getTagName());
 		 driver.get("http://demo.guru99.com/test/newtours/");
+		 driver.manage().window().maximize();
 		 System.out.println(driver.findElement(By.cssSelector("font:contains('Password:')")).getTagName());
 		 driver.quit();
 
